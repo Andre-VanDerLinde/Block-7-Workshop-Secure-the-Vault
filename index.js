2c68bdedce164a8565785e8235806eb121388ddf
Andre-VanDerLinde <andrevdl.engineering@gmail.com>
2025-02-04 18:29:37 -0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// Step 1: Create a string for the user saying: You have received this message because you have been chosen to open an important vault. Here is the secret combination:
+const prompt = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+// Step 2: Display prompt using alert popup
+alert(prompt);
+
+//Step 4: using mathematical operator - to calculate for 10; 
+//assigning result to a variable
+const num1 = 15 - 5;
+
+//Step 5: using mathematical operator * to calculate for 40;
+//assigning result to a variable
+const num2 = 20 * 2;
+
+//Step 6: using mathematical operator + to calculate for 39;
+//assigning result to a variable 
+const num3 = 25 + 14;
+
+//Step 7:Creating a popup dialog to display vault codes
+//using backticks and assigned variables
+alert(`Vault Codes: ${num1}-${num2}-${num3}`);
